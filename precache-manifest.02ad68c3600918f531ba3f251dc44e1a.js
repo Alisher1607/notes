754928a79731d6ccb8f88d4a6585be44fb33cc1c
Alisher <zumanovaliser814@gmail.com>
2022-11-16 21:48:38 +0600

    Updates

---
+++ precache-manifest.02ad68c3600918f531ba3f251dc44e1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a171a2845227590c386681ed0c4a4c1",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "b03ce33301cf5f330c95",
+    "url": "/notes/static/css/main.fc5fa3b9.chunk.css"
+  },
+  {
+    "revision": "200e341188183ef41efb",
+    "url": "/notes/static/js/2.19e33928.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/notes/static/js/2.19e33928.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b03ce33301cf5f330c95",
+    "url": "/notes/static/js/main.64a3a60e.chunk.js"
+  },
+  {
+    "revision": "274f754a4a777716979f",
+    "url": "/notes/static/js/runtime-main.38631402.js"
+  }
+]);
